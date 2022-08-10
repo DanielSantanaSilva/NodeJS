6d029460c5767f3527e774a5a2a3ea9b92955611
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-08-10 17:12:32 -0300

    alteração do chalk
    Alterado versão do chalk para consertar o erro de importação dos modulos da lib externa (utilizado versão 4.1.2 do chalk).

--- cli.js
+++ cli.js
@@ -1,12 +1,13 @@
-import pegaArquivo from "pegaArquivo";
-//const pegaArquivo = require("./index");
+const chalk = require('chalk');
+const pegaArquivo = require('./index');
+
 const caminho = process.argv;
 
-// Interface de linha de comando
+async function processaTexto(caminhoDeArquivo) {
+  const resultado = await pegaArquivo(caminhoDeArquivo[2]);
+  console.log(chalk.yellow('lista de links'), resultado);
+}
+
+processaTexto(caminho);
 
 //cli é command line interface, interface de linha de comando, na verdade nada mais é do que o nosso terminal que é a interface onde inserimos, onde fazemos o input de comandos na linha de comando.
-//import pegaArquivo from "pegaArquivo";
-
-console.log(pegaArquivo(caminho[2]));
-
-//./texto1.md
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import chalk from "chalk";
-import fs from "fs";
+const chalk = require("chalk");
+const fs = require("fs");
 
 function extraiLinks(texto) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
@@ -8,25 +8,24 @@ function extraiLinks(texto) {
   while ((temp = regex.exec(texto)) !== null) {
     arrayResultados.push({ [temp[1]]: temp[2] });
   }
-
-  return arrayResultados;
+  return arrayResultados.length === 0 ? "não há links" : arrayResultados;
 }
 
 function trataErro(erro) {
-  throw new Error(chalk.red(erro.code, "Não há arquivo no caminho"));
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro);
   }
 }
 
-export default pegaArquivo;
+module.exports = pegaArquivo;
 
 //pegaArquivo("./texto1.md");
 //module.exports = pegaArquivo;
